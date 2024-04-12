cf4a28c761a0e0d9d3fd1cb181359b7662c7f41d
Sooblmoob <selma.almqvist@gmail.com>
2024-04-12 11:29:39 +0200

    menu anchor fixes
    hoppas hoppas hoppas den inte e fucked :)

--- Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/First Person Player/MouseLook.cs
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/First Person Player/MouseLook.cs
@@ -23,14 +23,18 @@ namespace SojaExiles
         // Update is called once per frame
         void Update()
         {
-            float mouseX = Input.GetAxis("Mouse X") * mouseXSensitivity;
-            float mouseY = Input.GetAxis("Mouse Y") * mouseXSensitivity;
+            if (Time.deltaTime != 0)
+            {
+                float mouseX = Input.GetAxis("Mouse X") * mouseXSensitivity;
+                float mouseY = Input.GetAxis("Mouse Y") * mouseXSensitivity;
 
-            xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+                xRotation -= mouseY;
+                xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-            playerBody.Rotate(Vector3.up * mouseX);
+                transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+                playerBody.Rotate(Vector3.up * mouseX);
+            }
         }
+            
     }
 }
