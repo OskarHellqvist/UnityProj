61da2795f40c8264d475c03c092db34680be8608
Neo Ferrari <neoferrari02@gmail.com>
2024-04-17 14:27:07 +0200

    Revert "Merge branch 'New_Main' of https://github.com/OskarHellqvist/UnityProj into New_Main"
    This reverts commit c5f9a22c8ba338bf4c9f97f386e157c0e666bafc, reversing
    changes made to 1f51452a80b0c2a90ab049ecf4adbd4fe420a81a.

--- Assets/Brick Project Studio/Apartment Kit/Common/Scripts & Animation/Windows/opencloseWindowApt.cs
+++ Assets/Brick Project Studio/Apartment Kit/Common/Scripts & Animation/Windows/opencloseWindowApt.cs
@@ -1,44 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Net.Sockets;
 using UnityEngine;
 
 namespace SojaExiles
 
 {
-	public class opencloseWindowApt : MonoBehaviour, Interactable
-    {
+	public class opencloseWindowApt : MonoBehaviour
+	{
 
 		public Animator openandclosewindow;
 		public bool open;
 		public Transform Player;
 
-        public bool locked = false;
-
-        void Start()
+		void Start()
 		{
 			open = false;
 		}
 
-        public void Interact()
-        {
-            if (open == false && !locked)
-            {
-                StartCoroutine(opening());
-            }
-            else if (open == true)
-            {
-                StartCoroutine(closing());
-            }
-        }
+		void OnMouseOver()
+		{
+			{
+				if (Player)
+				{
+					float dist = Vector3.Distance(Player.position, transform.position);
+					if (dist < 3)
+					{
+						if (open == false)
+						{
+							if (Input.GetMouseButtonDown(0))
+							{
+								StartCoroutine(opening());
+							}
+						}
+						else
+						{
+							if (open == true)
+							{
+								if (Input.GetMouseButtonDown(0))
+								{
+									StartCoroutine(closing());
+								}
+							}
 
-        public void OpenDoor() { StartCoroutine(opening()); }
-        public void CloseDoor() { StartCoroutine(closing()); }
-        public void LockDoor() { locked = true; }
-        public void UnlockDoor() { locked = false; }
-        public void LockUnlockDoor() { locked = !locked; }
+						}
 
-        IEnumerator opening()
+					}
+				}
+
+			}
+
+		}
+
+		IEnumerator opening()
 		{
 			print("you are opening the Window");
 			openandclosewindow.Play("Openingwindow");
