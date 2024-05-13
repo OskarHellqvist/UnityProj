dab6fed296093fd66d9cfde05edbd655acad3b4e
Neo Ferrari <neoferrari02@gmail.com>
2024-05-13 13:42:52 +0200

    Death

---
+++ Assets/Global.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+
+namespace SojaExiles
+{
+    public static class Global
+    {
+
+        public static void LoadSceneStartMenu()
+        {
+            SceneManager.LoadScene(0);
+        }
+        public static void LoadSceneIntro()
+        {
+            SceneManager.LoadScene(1);
+        }
+        public static void LoadSceneGame()
+        {
+            SceneManager.LoadScene(2);
+        }
+        public static void LoadSceneWin()
+        {
+            SceneManager.LoadScene(3);
+        }
+        public static void LoadSceneGameOver()
+        {
+            SceneManager.LoadScene(3);
+
+        }
+    }
+}
