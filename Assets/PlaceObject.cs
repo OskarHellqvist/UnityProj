fe7e3121dc6d2b3caef963698f6d88e6e48cfbaa
OskarHellqvist <korven772@gmail.com>
2024-05-13 23:03:10 +0200

    skibidi sigma
    :de:

---
+++ Assets/PlaceObject.cs
@@ -0,0 +1,24 @@
+using SojaExiles;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceObject : MonoBehaviour, Interactable
+{
+    public GameObject objectToPlace;
+
+    public void Interact()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
