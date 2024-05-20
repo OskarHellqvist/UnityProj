4695f2208502d7324d3c544023348d2bd3bf3e30
OskarHellqvist <korven772@gmail.com>
2024-05-20 18:53:27 +0200

    WinConditionDone
    yaaaaaaa baby

--- Assets/TvScript.cs
+++ Assets/TvScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TvScript : MonoBehaviour
 {
     BoxCollider collider;
+    public GameObject videoScreen;
 
     void Start()
     {
@@ -21,10 +22,8 @@ public class TvScript : MonoBehaviour
 
     IEnumerator ActivateTvEventTemporarily()
     {
-        EventManager.manager.tvEvent.Invoke(); // Invoke the event
-        yield return new WaitForSeconds(5);    // Wait for 5 seconds
-        // You can add code here to deactivate the event or handle the aftermath
-        //Debug.Log("TV event deactivated");
-        Destroy(gameObject); // Optionally destroy the object after the event is over
+        EventManager.manager.tvEvent.Invoke(); 
+        Destroy(gameObject); 
+        yield return new WaitForSeconds(5);    
     }
 }
